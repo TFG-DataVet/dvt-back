6db2e0ff2be438be8730a2bae2365a7f5e994beb
Jesus Santiago <jesusantiago420@gmail.com>
2026-01-14 22:49:43 +0100

    DV-199 - ADD DiagnosisDetails

---
+++ src/main/java/com/datavet/datavet/pet/domain/model/details/diagnosis/DiagnosisCategory.java
@@ -0,0 +1,15 @@
+package com.datavet.datavet.pet.domain.model.details.diagnosis;
+
+public enum DiagnosisCategory {
+    INFECTIOUS,
+    PARASITIC,
+    GENETIC,
+    METABOLIC,
+    NEUROLOGICAL,
+    DERMATOLOGICAL,
+    ORTHOPEDIC,
+    CARDIOVASCULAR,
+    RESPIRATORY,
+    DIGESTIVE,
+    OTHER
+}
