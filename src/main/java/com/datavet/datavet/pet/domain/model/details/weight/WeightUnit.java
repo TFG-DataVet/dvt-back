23b701064f25fb7e1ab3549afe68892cc8b9f6f2
Jesus Santiago <jesusantiago420@gmail.com>
2026-01-15 20:37:55 +0100

    DV-201 fix details routes

---
+++ src/main/java/com/datavet/datavet/pet/domain/model/details/weight/WeightUnit.java
@@ -0,0 +1,6 @@
+package com.datavet.datavet.pet.domain.model.details.weight;
+
+public enum WeightUnit {
+    KG,
+    LB
+}
--- src/main/java/com/datavet/datavet/pet/domain/valueobject/WeightUnit.java
+++
@@ -1,6 +0,0 @@
-package com.datavet.datavet.pet.domain.valueobject;
-
-public enum WeightUnit {
-    KG,
-    LB
-}
