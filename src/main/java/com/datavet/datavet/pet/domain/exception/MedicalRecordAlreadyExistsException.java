61d66d35e327739279c944f7793361dfb0973b9c
Jesus Santiago <jesusantiago420@gmail.com>
2026-03-04 22:28:54 +0100

    DV-217 new exceptions

---
+++ src/main/java/com/datavet/datavet/pet/domain/exception/MedicalRecordAlreadyExistsException.java
@@ -0,0 +1,18 @@
+package com.datavet.datavet.pet.domain.exception;
+
+import com.datavet.datavet.shared.domain.exception.EntityAlreadyExistsException;
+
+public class MedicalRecordAlreadyExistsException extends EntityAlreadyExistsException {
+
+    public MedicalRecordAlreadyExistsException(String message) {
+        super(message);
+    }
+
+    public MedicalRecordAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MedicalRecordAlreadyExistsException(String fieldName, String fieldValue) {
+        super("MedicalRecord", fieldName, fieldValue);
+    }
+}
---
+++ src/main/java/com/datavet/datavet/pet/domain/exception/MedicalRecordApplyActionException.java
@@ -0,0 +1,34 @@
+package com.datavet.datavet.pet.domain.exception;
+
+import com.datavet.datavet.shared.domain.validation.ValidationResult;
+
+public class MedicalRecordApplyActionException extends MedicalRecordDomainException {
+
+    private final ValidationResult validationResult;
+
+    public MedicalRecordApplyActionException(ValidationResult validationResult) {
+        super("Medical record apply action failed" + formatErrors(validationResult));
+        this.validationResult = validationResult;
+    }
+
+    public MedicalRecordApplyActionException(String field, String message) {
+        this(buildResult(field, message));
+    }
+
+    private static ValidationResult buildResult(String field, String message) {
+        ValidationResult result = new ValidationResult();
+        result.addError(field, message);
+        return result;
+    }
+
+    public ValidationResult getValidationResult() {
+        return validationResult;
+    }
+
+    public static String formatErrors(ValidationResult result) {
+        return result.getErrors().stream()
+                .map(error -> error.getField() + ": " + error.getMessage())
+                .reduce((a, b) -> a + ", " + b)
+                .orElse("Unknown validation error");
+    }
+}
